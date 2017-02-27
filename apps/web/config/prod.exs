c0ad4fe9d6352346344c2ea2e0df28b336a120ae
Axel Clark <axelclark2@yahoo.com>
2017-02-26 19:10:45 -0500

    Remove static asset processing from config
    * Remove static asset processing from config

--- apps/web/config/prod.exs
+++ apps/web/config/prod.exs
@@ -15,7 +15,6 @@ config :web, Web.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "dry-shelf-59131.herokuapp.com/", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
