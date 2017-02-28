cd6b2a1191a03c237733cab37a7ec18dda00117a
Axel Clark <axelclark2@yahoo.com>
2017-02-27 19:47:05 -0500

    Handle WhatsMyColorIntent without favorite color
    * Handle WhatsMyColorIntent when there is no favoriteColor in the session
    * Should inform the user how to tell Alexa about her favorite color
    * Closes #14

--- apps/alexa_skill/lib/alexa_skill/color_picker/whats_my_color_intent.ex
+++ apps/alexa_skill/lib/alexa_skill/color_picker/whats_my_color_intent.ex
@@ -7,6 +7,10 @@ defmodule AlexaSkill.ColorPicker.WhatsMyColorIntent do
     AlexaJSON.Response.update_response(response, update)
   end
 
+  def handle_request(_request, response) do
+    AlexaJSON.Response.update_response(response, no_color())
+  end
+
   defp create_update(color) do
     %{
       response: %{
@@ -33,5 +37,25 @@ defmodule AlexaSkill.ColorPicker.WhatsMyColorIntent do
 
   defp create_content(color) do
     "SessionSpeechlet - Your favorite color is #{color}. Goodbye."
+  end
+
+  defp no_color() do
+    %{
+      response: %{
+        shouldEndSession: false,
+        outputSpeech: %{
+          type: "PlainText",
+          text: "I'm not sure what your favorite color is. You can say, my favorite color is red."
+        },
+        card: %{
+          content: "SessionSpeechlet - I'm not sure what your favorite color is. You can say, my favorite color is red.",
+          title: "SessionSpeechlet - WhatsMyColorIntent",
+          type: "Simple"
+        },
+        reprompt: %{}
+      },
+      sessionAttributes: %{},
+      version: "1.0"
+    }
   end
 end
--- apps/alexa_skill/test/lib/alexa_skill/color_picker/whats_my_color_intent_test.exs
+++ apps/alexa_skill/test/lib/alexa_skill/color_picker/whats_my_color_intent_test.exs
@@ -38,5 +38,40 @@ defmodule AlexaSkill.ColorPicker.WhatsMyColorIntentTest do
       assert result.response.outputSpeech.text ==
         "Your favorite color is red. Goodbye."
     end
+
+    test "returns response to when no favorite color in session" do
+      request =
+        %AlexaJSON.Request{
+          request: %AlexaJSON.RequestElement{
+            inDialog: false,
+            intent: %AlexaJSON.Intent{
+              name: "WhatsMYColorIntent",
+              slots: %{}
+            },
+            locale: "en-US",
+            requestId: "request123",
+            timestamp: nil,
+            type: "IntentRequest"
+          },
+          session: %AlexaJSON.Session{
+            application: %AlexaJSON.Application{
+              applicationId: "appId123"
+            },
+            attributes: %{},
+            new: true,
+            sessionId: "session123",
+            user: %AlexaJSON.User{userId: "user123"}
+          },
+          version: "1.0"
+        }
+      response = AlexaJSON.Response.create_from_request(request)
+
+      result = WhatsMyColorIntent.handle_request(request, response)
+
+      assert result.response.outputSpeech.text ==
+        "I'm not sure what your favorite color is. You can say, my favorite color is red."
+      assert result.response.card.content ==
+        "SessionSpeechlet - I'm not sure what your favorite color is. You can say, my favorite color is red."
+    end
   end
 end
