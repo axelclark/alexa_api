c8220e43339d7b6d1f210e58c84c38f6b8c830e2
Axel Clark <axelclark2@yahoo.com>
2017-02-27 20:49:58 -0500

    Create WMATA app
    * Create WMATA app to make https request to WMATA
    * See issue #17

---
+++ apps/wmata/lib/wmata.ex
@@ -0,0 +1,18 @@
+defmodule WMATA do
+  @moduledoc """
+  Documentation for WMATA.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WMATA.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/wmata/test/wmata_test.exs
@@ -0,0 +1,8 @@
+defmodule WMATATest do
+  use ExUnit.Case
+  doctest WMATA
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
