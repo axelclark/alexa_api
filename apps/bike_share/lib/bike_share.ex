4d672e2d938ad93e18a97bb0f498e673aa017bf3
Axel Clark <axelclark2@yahoo.com>
2017-03-04 08:33:58 -0500

    Create app for Capital Bike Share

---
+++ apps/bike_share/lib/bike_share.ex
@@ -0,0 +1,18 @@
+defmodule BikeShare do
+  @moduledoc """
+  Documentation for BikeShare.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BikeShare.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/bike_share/test/bike_share_test.exs
@@ -0,0 +1,8 @@
+defmodule BikeShareTest do
+  use ExUnit.Case
+  doctest BikeShare
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
